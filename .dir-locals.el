abb8d0cf86b66d4a821c3fa81830a8d3ac6f2caa
Karl Hallsby <karl@hallsby.com>
2023-07-05 00:03:22 -0500

    Add .dir-locals.el to start configuration

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((fill-column . 80)
+         (indent-tabs-mode . nil)))
+ (makefile-mode . ((indent-tabs-mode . t)))
+ (common-lisp-mode
+  .
+  ((indent-tabs-mode . nil))))
